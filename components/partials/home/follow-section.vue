e48c9b22eb4d5455c218ea27cd07707e3c23a6a2
Théotime Colin <contact@theotimecolin.com>
2020-09-02 08:41:23 +0200

    feat(Homepage): follow section

--- components/partials/home/follow-section.vue
+++ components/partials/home/follow-section.vue
@@ -1,8 +1,21 @@
 <template>
     <div class="Homepage_follow">
-        <div class="Wrapper Wrapper--2xs text-center pt-20 pb-100">
-            <h2 class="ft-title-2xl ft-bold color-blueberry mb-40">De nouveaux événements toutes les semaines</h2>
-            <p>Tu n'as pas trouvé ton bonheur ? Reste informé(e) : nous avons des événement récurrents pour tous les goûts, et toujours des nouveautés !</p>
+        <div class="Wrapper pv-100">
+            <div class="row">
+                <div class="col-6">
+                    <h2 class="ft-title-xl ft-bold color-cherry-weak mb-30">
+                        De nouveaux événements<br>
+                        <span class="ft-title-4xl ft-bold">toutes les semaines</span>
+                    </h2>
+
+                    <p><b>Tu n'as pas trouvé ton bonheur ?</b> Reste informé(e) : nous avons toujours des événements en préparation pour tous les goûts, et de belles surprises !</p>
+
+                    <div class="mt-30">
+                        <button-base link="https://www.meetup.com/fr-FR/antiswipe-lgbt">Nous rejoindre</button-base>
+                        <button-base class="ml-5" :modifiers="['secondary', 'blueberry', 's']" link="https://www.meetup.com/fr-FR/antiswipe-lgbt">Nous suivre sur Instagram</button-base>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
