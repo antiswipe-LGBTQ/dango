98d2db0fa342d7e0a04e92a097c40c92b7396050
Théotime COLIN <contact@theotimecolin.com>
2020-12-01 18:36:27 +0100

    feat(Pages): get dynamic pages

---
+++ pages/pages/_slug.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-if="page">
+        <div class="Wrapper mt-100">
+            {{ page.title }}
+        </div>
+
+        <div class="Wrapper Wrapper--s">
+            <div v-html="page.content"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GenericPage',
+    async fetch () {
+        await this.$store.dispatch('pages/get', { query: { slug: this.$route.params.slug }})
+    },
+    computed: {
+        page () {
+            return this.$store.getters['pages/findOne']({
+                slug: this.$route.params.slug
+            })
+        }
+    }
+}
+</script>
---
+++ store/pages.js
@@ -0,0 +1,51 @@
+import storeUtils from '@/utils/store-utils'
+import moment from 'moment'
+
+export default {
+    namespaced: true,
+    state: () => ({
+        items: {}
+    }),
+    mutations: {
+        updateOne (state, value) {
+            state.items = storeUtils.updateOne(state, value)
+        },
+        refresh (state, values) {
+            state.items = storeUtils.refresh(values)
+        }
+    },
+    actions: {
+        async get ({ commit }, params) {
+            try {
+                const response = await this.$axios.$get(storeUtils.getQuery('/entities', {
+                    ...params.query,
+                    type: 'page'
+                }))
+
+                commit('updateOne', response.data[0])
+
+                return response.data
+            } catch (e) {
+                console.error(e)
+                return null
+            }
+        },
+    },
+    getters: {
+        items: (state) => {
+            return Object.values(state.items).map(item => {
+                return {
+                    ...item
+                }
+            })
+        },
+        find: (state, getters) => (search, raw = false) => {
+            let items = raw ? Object.values(state.items) : getters.items
+            return items
+        },
+        findOne: (state, getters) => (search, raw = false) => {
+            let items = raw ? Object.values(state.items) : getters.items
+            return items.find(item => item[Object.keys(search)[0]] == Object.values(search)[0])
+        }
+    }
+}
